9bcd310d73b1dff8e120fb048b2c1457ae2497af
yugansh003 <yugansh.4waytechnologies@gmail.com>
2026-03-19 23:47:05 +0530

    feat: add global styles, layout, and hero component with Tailwind CSS integration

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between bg-white dark:bg-black ">
+      </main>
+    </div>
+  );
+}
